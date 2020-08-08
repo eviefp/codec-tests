202d5657454897501f8e0ecc3ff8953b1a35ec03
Vladimir Ciobanu <admin@cvlad.info>
2020-08-09 01:19:02 +0300

    End of stream 09 aug.

--- src/Codec.hs
+++ src/Codec.hs
@@ -1,12 +1,14 @@
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE ExplicitForAll #-}
+
 module Codec where
 
-import Control.Monad(join)
-import Data.Functor.Invariant (Invariant2(invmap2))
-import Data.Profunctor (Star(Star, runStar), dimap)
-import Prelude
-import Control.Category (Category)
-import Control.Applicative (Alternative(..))
-import qualified Control.Category as C
+import Control.Monad ( join )
+import Data.Functor.Invariant ( Invariant2(invmap2) )
+import Data.Profunctor ( Star(Star, runStar), dimap )
+import Prelude hiding ( (.), id )
+import Control.Category
+import Control.Applicative ( Alternative(..) )
 
 -- | A codec is a bi-directional parser. It's encoded using Star, but it's
 -- essentially a pair of functions (i -> m o) and (o -> n i) for decoding and
@@ -19,54 +21,42 @@ import qualified Control.Category as C
 -- 'o :: *' is the output type.
 data Codec m n i o = Codec (Star m i o) (Star n o i)
 
+-- Simple helper function to create codecs.
+mkCodec :: (a -> m b) -> (b -> n a) -> Codec m n a b
+mkCodec decoder encoder = Codec (Star decoder) (Star encoder)
+
 -- Map over 'i' and 'o'.
-instance (Functor m, Functor n) => Invariant2 (Codec m n) where
-  invmap2
-      :: (a -> c)
-      -> (c -> a)
-      -> (b -> d)
-      -> (d -> b)
-      -> Codec m n a b
-      -> Codec m n c d
-  invmap2 a2c c2a b2d d2b (Codec mab nba) =
-    Codec (dimap c2a b2d mab) (dimap d2b a2c nba)
+instance ( Functor m, Functor n ) => Invariant2 (Codec m n) where
+    invmap2 :: (a -> c) -> (c -> a) -> (b -> d) -> (d -> b)
+        -> Codec m n a b -> Codec m n c d
+    invmap2 a2c c2a b2d d2b (Codec mab nba) = Codec (dimap c2a b2d mab)
+        (dimap d2b a2c nba)
 
 -- Map over 'm' and 'n'.
-hoist
-  :: (forall x. m x -> m' x)
-  -> (forall x. n x -> n' x)
-  -> Codec m n a b
-  -> Codec m' n' a b
-hoist f g (Codec mab nba) =
-    Codec
-        (Star $ f . runStar mab)
-        (Star $ g . runStar nba)
+hoist :: (forall x. m x -> m' x) -> (forall x. n x -> n' x)
+    -> Codec m n a b -> Codec m' n' a b
+hoist f g (Codec mab nba) = Codec (Star $ f . runStar mab)
+    (Star $ g . runStar nba)
 
 -- Monadic composition for when short-circuing is fine.
-instance (Monad m, Monad n) => Category (Codec m n) where
-  id :: Codec m n a a
-  id = Codec C.id C.id
+instance ( Monad m, Monad n ) => Category (Codec m n) where
+    id :: Codec m n a a
+    id = Codec id id
 
-  (.) :: Codec m n b c -> Codec m n a b -> Codec m n a c
-  (.) (Codec mbc ncb) (Codec mab nba) =
-      Codec (mbc C.. mab) (nba C.. ncb)
+    (.) :: Codec m n b c -> Codec m n a b -> Codec m n a c
+    (.) (Codec mbc ncb) (Codec mab nba) = Codec (mbc . mab) (nba . ncb)
 
-manualIdentity
-    :: Applicative m
-    => Applicative n
-    => Codec m n a a
+-- Since Category requires 'm' and 'n' to be Monads, we provide stand-alone
+-- functions for identity and composition (pure and bind) for when
+-- short-circuiting is not acceptable (e.g. 'Validation' parsing).
+manualIdentity :: Applicative m => Applicative n => Codec m n a a
 manualIdentity = Codec (Star pure) (Star pure)
 
--- Manual composition for things like 'Validation'.
-manualCompose
-    :: forall m n a b c
-    .  (forall x y. m x -> (x -> m y) -> m y)
+manualCompose :: forall m n a b c. (forall x y. m x -> (x -> m y) -> m y)
     -> (forall x y. n x -> (x -> n y) -> n y)
-    -> Codec m n b c
-    -> Codec m n a b
-    -> Codec m n a c
-manualCompose bindM bindN (Codec mbc ncb) (Codec mab nba) =
-    Codec (Star mac) (Star nca)
+    -> Codec m n b c -> Codec m n a b -> Codec m n a c
+manualCompose bindM bindN (Codec mbc ncb) (Codec mab nba) = Codec (Star mac)
+    (Star nca)
   where
     mac :: a -> m c
     mac a = runStar mab a `bindM` runStar mbc
@@ -74,34 +64,21 @@ manualCompose bindM bindN (Codec mbc ncb) (Codec mab nba) =
     nca :: c -> n a
     nca c = runStar ncb c `bindN` runStar nba
 
-mkCodec :: (a -> m b) -> (b -> n a) -> Codec m n a b
-mkCodec decoder encoder = Codec (Star decoder) (Star encoder)
-
-(***)
-    :: forall m n a b c
-    .  Applicative m
-    => Applicative n
-    => Semigroup a
-    => Codec m n a b
-    -> Codec m n a c
-    -> Codec m n a (b, c)
-(***) (Codec mab nba) (Codec mac nca) =
-    Codec (Star mabc) (Star nbca)
+(***) :: forall m n a b c. Applicative m => Applicative n
+    => Semigroup a => Codec m n a b -> Codec m n a c -> Codec m n a ( b, c )
+(***) (Codec mab nba) (Codec mac nca) = Codec (Star mabc) (Star nbca)
   where
-    mabc :: a -> m (b, c)
+    mabc :: a -> m ( b, c )
     mabc a = (,) <$> runStar mab a <*> runStar mac a
 
-    nbca :: (b, c) -> n a
-    nbca (b, c) = (<>) <$> runStar nba b <*> runStar nca c
+    nbca :: ( b, c ) -> n a
+    nbca ( b, c ) = (<>) <$> runStar nba b <*> runStar nca c
 
-(//)
-    :: forall m n a b c
-    .  Alternative m
-    => Codec m n a b
-    -> Codec m n a c
+-- Try parsing 'b' and tag it with 'Left' if it succeeds. Parse it with 'c'
+-- otherwise and tag it with 'Right'.
+(//) :: forall m n a b c. Alternative m => Codec m n a b -> Codec m n a c
     -> Codec m n a (Either b c)
-(//) (Codec mab nba) (Codec mac nca) =
-    Codec (Star mabc) (Star nbca)
+(//) (Codec mab nba) (Codec mac nca) = Codec (Star mabc) (Star nbca)
   where
     mabc :: a -> m (Either b c)
     mabc a = Left <$> runStar mab a <|> Right <$> runStar mac a
@@ -110,86 +87,54 @@ mkCodec decoder encoder = Codec (Star decoder) (Star encoder)
     nbca = either (runStar nba) (runStar nca)
 
 -- This is not very useful :(
-both
-    :: forall m n a b c d
-    .  Monad m
-    => Monad n
-    => Alternative m
-    => Codec m n a (b, c)
-    -> Codec m n b d
-    -> Codec m n c d
-    -> Codec m n a d
-both
-  (Codec (Star a2mbc) (Star bc2na))
-  (Codec (Star b2md) (Star d2nb))
-  (Codec (Star c2md) (Star d2nc)) =
-    Codec (Star a2md) (Star d2na)
+-- Could this be implemented using the 'Category' instance?
+-- both
+--     :: forall m n a b c d e
+--     .  Monad m
+--     => Monad n
+--     => Codec m n a ( b, c )
+--     -> Codec m n b d
+--     -> Codec m n c e
+--     -> Codec m n a (d, e)
+-- both (Codec (Star a2mbc) (Star bc2na)) (Codec (Star b2md) (Star d2nb))
+--     (Codec (Star c2md) (Star d2nc)) = Codec (Star a2md) (Star d2na)
+--   where
+--     a2md :: a -> m d
+--     a2md a = a2mbc a >>= \( b, c ) -> b2md b <|> c2md c
+
+--     d2na :: d -> n a
+--     d2na d = join $ curry bc2na <$> d2nb d <*> d2nc d
+
+(^^^) :: forall m n b c d e. Applicative m => Applicative n
+    => Codec m n b d -> Codec m n c e -> Codec m n ( b, c ) ( d, e )
+(^^^) (Codec (Star b2md) (Star d2nb)) (Codec (Star c2me) (Star e2nc)) = Codec
+    (Star bc2mde) (Star de2nbc)
   where
-    a2md :: a -> m d
-    a2md a = a2mbc a >>= \(b, c) -> b2md b <|> c2md c
+    bc2mde :: ( b, c ) -> m ( d, e )
+    bc2mde ( b, c ) = (,) <$> b2md b <*> c2me c
 
-    d2na :: d -> n a
-    d2na d = join $ curry bc2na <$> d2nb d <*> d2nc d
+    de2nbc :: ( d, e ) -> n ( b, c )
+    de2nbc ( d, e ) = (,) <$> d2nb d <*> e2nc e
 
-(^^^)
-    :: forall m n b c d e
-    .  Applicative m
-    => Applicative n
-    => Codec m n b d
-    -> Codec m n c e
-    -> Codec m n (b, c) (d, e)
-(^^^) (Codec (Star b2md) (Star d2nb)) (Codec (Star c2me) (Star e2nc)) =
-    Codec (Star bc2mde) (Star de2nbc)
-  where
-    bc2mde :: (b, c) -> m (d, e)
-    bc2mde (b, c) = (,) <$> b2md b <*> c2me c
-
-    de2nbc :: (d, e) -> n (b, c)
-    de2nbc (d, e) = (,) <$> d2nb d <*> e2nc e
-
--- Codec m n Attribute Currency
--- Codec m n Attribute Bank
-
--- Codec m n Node [Attribute]
---    -> Codec m n [Attribute] (Attribute, Attribute)
-
--- Codec m n i [Attribute] *** Codec m n i [XmlNode]
---     = Codec m n i ([Attribute], [XmlNode]
--- Codec m n [Attribute] T `thing` Codec m n [XmlNode] U
---     = Codec m n ([Attribute], [XmlNode]) T
-
--- Currency from Attribute
--- Amount from XmlNode
-
--- <payment currency="USD">123</payment>
--- <currency from="USD" bank="SomeBank">123</currency>
-
--- try this out:
--- Codec m n a b -> Codec m n c d -> Codec m n (a, c) (b, d)
-
+-- Think about whether we could return an isomorphism instead of ->.
 class Inapplicative f where
-  inpure :: a -> b -> f a b
-  -- (<**>) :: f (a -> b) (c -> d) -> f x c -> f x d -- nope :(
-  lift2 :: ((a, b) -> c) -> (c -> (a, b)) -> f x a -> f x b -> f x c
+    -- inpure :: a -> b -> f a b
+    -- (<**>) :: f (a -> b) (c -> d) -> f x c -> f x d -- nope :(
+    lift2 :: (( a, b ) -> c) -> (c -> ( a, b )) -> f a -> f b -> f c
 
-instance (Applicative m, Alternative n) => Inapplicative (Codec m n) where
-  inpure :: a -> b -> Codec m n a b
-  inpure a b = Codec (pure b) (pure a)
+  -- good ol' lift2 :: (a -> b -> c) -> f a -> f b -> f c
+  -- good ol' <*>   :: f (a -> b) -> f a -> f b
+instance ( Applicative m, Alternative n ) => Inapplicative (Codec m n x) where
+    -- inpure :: a -> b -> Codec m n a b
+    -- inpure a b = Codec (pure b) (pure a)
+    lift2 :: forall a b c. (( a, b ) -> c) -> (c -> ( a, b ))
+        -> Codec m n x a -> Codec m n x b -> Codec m n x c
+    lift2 ab2c c2ab (Codec (Star x2ma) (Star a2nx))
+        (Codec (Star x2mb) (Star (b2nx))) = Codec (Star x2mc) (Star c2nx)
+      where
+        x2mc :: x -> m c
+        x2mc x = curry ab2c <$> x2ma x <*> x2mb x
 
-  lift2
-      :: forall a b c x
-      .  ((a, b) -> c)
-      -> (c -> (a, b))
-      -> Codec m n x a
-      -> Codec m n x b
-      -> Codec m n x c
-  lift2 ab2c c2ab (Codec (Star x2ma) (Star a2nx)) (Codec (Star x2mb) (Star (b2nx))) =
-      Codec (Star x2mc) (Star c2nx)
-    where
-      x2mc :: x -> m c
-      x2mc x = curry ab2c <$> x2ma x <*> x2mb x
+        c2nx :: c -> n x
+        c2nx c = let ( a, b ) = c2ab c in a2nx a <|> b2nx b
 
-      c2nx :: c -> n x
-      c2nx c =
-        let (a, b) = c2ab c
-        in a2nx a <|> b2nx b
--- src/XmlCodecTake2.hs
+++ src/XmlCodecTake2.hs
@@ -1,83 +1,141 @@
+{-# LANGUAGE ExplicitForAll #-}
+
 module XmlCodecTake2 where
 
-import Control.Applicative (Alternative, empty)
-import Data.Functor.Invariant (Invariant2(invmap2))
+import Control.Applicative ( Alternative, empty )
+import Data.Functor.Invariant ( Invariant2(invmap2) )
 import Codec
 import Prelude
-import Data.Functor.Identity (Identity)
-import Control.Category ((>>>))
-import qualified Control.Category as C
+import Data.Functor.Identity ( Identity )
+import Control.Category ( (>>>) )
+import Data.Profunctor ( Star(Star) )
 
 type XmlCodec a b = Codec Maybe Identity a b
 
-data XmlNode
-    = Node String [Attribute] [XmlNode]
-    | Text String
+data XmlNode = Node String [ Attribute ] [ XmlNode ] | Text String
+    deriving stock Eq
 
 data Attribute = Attribute String String
+    deriving stock Eq
 
 example :: XmlNode
 example =
-    Node "parent"
-        [ Attribute "key" "value" ]
+    Node
+        "parent"
+        [ Attribute "key1" "value1"
+        , Attribute "key2" "value2"
+        ]
         []
 
-element :: XmlCodec XmlNode (String, [Attribute])
+element :: XmlCodec XmlNode ( String, ([ Attribute ], [XmlNode] ))
 element = mkCodec decode encode
   where
-    decode :: XmlNode -> Maybe (String, ([Attribute]))
-    decode (Node tag attrs _) = Just (tag, (attrs))
+    decode :: XmlNode -> Maybe ( String, ([ Attribute ], [XmlNode] ))
+    decode (Node tag attrs xs) = Just ( tag, ((attrs), xs))
     decode (Text _) = Nothing
 
-    encode :: (String, ([Attribute])) -> Identity XmlNode
-    encode (tag, attrs) = pure $ Node tag attrs []
+    encode :: ( String, ([ Attribute ], [XmlNode] )) -> Identity XmlNode
+    encode ( tag, (attrs, xs)) = pure $ Node tag attrs xs
 
-assert
-    :: forall a m n x
-    .  Eq a
-    => Alternative m
-    => Applicative n
-    => a
-    -> Codec m n (a, x) x
-assert a = mkCodec decode encode
+assertLeft :: forall a m n x.
+    Eq a => Alternative m => Applicative n => a -> Codec m n ( a, x ) x
+assertLeft a = mkCodec decode encode
   where
-    decode :: (a, x) -> m x
-    decode (a', x) = if a == a' then pure x else empty
+    decode :: ( a, x ) -> m x
+    decode ( a', x ) = if a == a' then pure x else empty
 
-    encode :: x -> n (a, x)
-    encode x = pure (a, x)
+    encode :: x -> n ( a, x )
+    encode x = pure ( a, x )
 
-attribute :: XmlCodec [Attribute] Attribute
+assertRight :: forall a m n x.
+    Eq a => Alternative m => Applicative n => a -> Codec m n ( x, a ) x
+assertRight a = mkCodec decode encode
+  where
+    decode :: ( x, a ) -> m x
+    decode ( x, a' ) = if a == a' then pure x else empty
+
+    encode :: x -> n ( x, a )
+    encode x = pure ( x, a )
+
+attribute :: XmlCodec [ Attribute ] Attribute
 attribute = mkCodec decode encode
   where
-    decode :: [Attribute] -> Maybe Attribute
-    decode [x] = Just x
+    decode :: [ Attribute ] -> Maybe Attribute
+    decode [ x ] = Just x
     decode _ = Nothing
 
-    encode :: Attribute -> Identity [Attribute]
+    encode :: Attribute -> Identity [ Attribute ]
     encode = pure . pure
 
-attributeText :: XmlCodec Attribute (String, String)
+attributes :: XmlCodec [Attribute] [(String, String)]
+attributes = mkCodec decode encode
+  where
+    decode :: [ Attribute ] -> Maybe [(String, String)]
+    decode = Just . fmap (\(Attribute k v) -> (k, v))
+
+    encode :: [(String, String)] -> Identity [Attribute]
+    encode = pure . fmap (uncurry Attribute)
+
+listCons :: forall a. XmlCodec [a] (a, [a])
+listCons = mkCodec decode encode
+  where
+    decode :: [a] -> Maybe (a, [a])
+    decode = \case
+        [] -> Nothing
+        (a:as) -> Just (a, as)
+
+    encode :: (a, [a]) -> Identity [a]
+    encode (a, as) = pure (a:as)
+
+assert :: forall a m n. Alternative m => Applicative n => Codec m n (Maybe a) a
+assert = Codec (Star fwd) (Star bwd)
+  where
+    fwd :: Maybe a -> m a
+    fwd = maybe empty pure
+    bwd :: a -> n (Maybe a)
+    bwd = pure . Just
+
+attributeText :: XmlCodec Attribute ( String, String )
 attributeText = mkCodec decode encode
   where
-    decode :: Attribute -> Maybe (String, String)
-    decode (Attribute key value) = Just (key, value)
+    decode :: Attribute -> Maybe ( String, String )
+    decode (Attribute key value) = Just ( key, value )
 
-    encode :: (String, String) -> Identity Attribute
-    encode (key, value) = pure $ Attribute key value
+    encode :: ( String, String ) -> Identity Attribute
+    encode ( key, value ) = pure $ Attribute key value
 
-data Thing = Thing String String String
+data Thing = Thing String String
+    deriving stock Show
 
-getKeyAttribute :: XmlCodec XmlNode (String, (String, String))
+getKeyAttribute :: XmlCodec XmlNode ( String, String  )
 getKeyAttribute =
-   element
-      >>> (C.id ^^^ (attribute >>> attributeText))
+    tuple >>> (left ^^^ right)
+  where
+    tuple :: XmlCodec XmlNode (Attribute, [Attribute])
+    tuple =
+        element
+            >>> assertLeft "parent"
+            >>> assertRight []
+            >>> listCons
+
+    left :: XmlCodec Attribute String
+    left = attributeText >>> assertLeft "key1"
+
+    right :: XmlCodec [Attribute] String
+    right =
+        listCons
+            >>> assertRight []
+            >>> attributeText
+            >>> assertLeft "key2"
 
 thingCodec :: XmlCodec XmlNode Thing
 thingCodec = invmap2 id id to from getKeyAttribute
   where
-    to :: (String, (String, String)) -> Thing
-    to (a, (b, c)) = Thing a b c
+    to :: (  String, String  ) -> Thing
+    to (  a, b )  = Thing a b
 
-    from :: Thing -> (String, (String, String))
-    from (Thing a b c) = (a, (b, c))
+    from :: Thing -> (  String, String  )
+    from (Thing a b) = ( a, b )
+
+runDecoder :: XmlCodec a b -> a -> Maybe b
+runDecoder (Codec (Star decode) _) a = decode a
